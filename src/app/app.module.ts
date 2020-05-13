b082786cec1b5c9f5f77369c8fa54f1c097c35c9
pshemkooo <pshemko1993@outlook.dk>
2020-05-13 09:20:34 +0200

    login but not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,19 +7,39 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {LoginComponent } from './login/login.component';
+import {MatCheckboxModule} from '@angular/material/checkbox'; 
+import {MatTabsModule} from '@angular/material/tabs'; 
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import { ItemComponent } from './item/item.component'; 
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCheckboxModule,
+    MatTabsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/services/item.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+const baseUrl = 'http://localhost:8080/item';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ItemService {
+  constructor(private http: HttpClient) {}
+
+  // Get all users
+  getAll() {
+    return this.http.get(baseUrl);
+  }
+
+  // Create a new user
+  create(data) {
+    return this.http.post(baseUrl, data);
+  }
+
+  // Get user by id
+  get(id) {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  // Update user information by id
+  update(id, data) {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  // Delete all users
+  deleteAll() {
+    return this.http.delete(baseUrl);
+  }
+
+  // Delete a user by id
+  delete(id) {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+
+}
