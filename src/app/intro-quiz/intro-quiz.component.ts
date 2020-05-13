965a71610054d1c8e611aa6881e4b04879558884
jakubjenigar <33846091+jakubjenigar@users.noreply.github.com>
2020-05-14 00:35:52 +0200

    Improved home page

---
+++ src/app/intro-quiz/intro-quiz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro-quiz',
+  templateUrl: './intro-quiz.component.html',
+  styleUrls: ['./intro-quiz.component.css']
+})
+export class IntroQuizComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
