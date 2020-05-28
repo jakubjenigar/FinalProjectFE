204d91d5191dc1ab64d1982eade4885b85e1d5cc
Miimkkaa <mile0416@edu.easj.dk>
2020-05-28 22:54:01 +0200

    tooltips

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,27 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {LoginComponent } from './login/login.component';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatIconModule} from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatTableModule} from '@angular/material/table';
-import {MatInputModule} from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { LoginComponent } from './login/login.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
 import { ItemComponent } from './item/item.component';
 import { SignupComponent } from './signup/signup.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './navbar/navbar.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FooterComponent } from './footer/footer.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CartComponent } from './cart/cart.component';
 import { ThanksComponent } from './thanks/thanks.component';
-import { MatSnackBarModule, MatNativeDateModule } from '@angular/material';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-
-
-
+import { MatSnackBarModule, MatTooltipModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -43,7 +39,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
     FooterComponent,
     QuizComponent,
     ThanksComponent,
-   
+
   ],
   imports: [
     BrowserModule,
@@ -67,8 +63,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
     MatToolbarModule,
     DragDropModule,
     MatSnackBarModule,
-    MatNativeDateModule,
-    MatDatepickerModule
+    MatTooltipModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
